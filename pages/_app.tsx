e715e44c239987415bc904ada8011151666a4da8
Shami_Mourya <shamimourya9852@gmail.com>
2023-12-03 18:17:58 +0530

    imported model

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout';
+// import Model from '@/components/Model';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
+    {/* <Model isOpen title='test model' actionLabel='Submit'/> */}
+     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
