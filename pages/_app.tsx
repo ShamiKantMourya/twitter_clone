e9f4fc35682bf8e4c5cb1acedd17e3fa46f2cbb0
Shami_Mourya <shamimourya9852@gmail.com>
2024-01-04 15:10:21 +0530

    toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
 import Layout from '@/components/Layout';
 import LoginModal from '@/components/modals/LoginModal';
@@ -8,13 +10,14 @@ import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-    {/* <Modal isOpen title='test model' actionLabel='Submit'/> */}
-    <RegisterModal />
-    <LoginModal />
-     <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    </>
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      {/* <Modal isOpen title='test model' actionLabel='Submit'/> */}
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   )
 }
